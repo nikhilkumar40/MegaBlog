72d8fcc9cc2faf10db6f56c92ad6c434d17c88ef
nikhilkumar40 <108416519+nikhilkumar40@users.noreply.github.com>
2025-08-02 17:29:14 +0530

    Protected name changed

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-function AuthLayout({children, authentication=true}) {
+export default function Protected({children, authentication=true}) {
 
     const authStatus = useSelector(state=>state.auth.isUserLoggedIn)
     const [loader, setLoader]= useState(true)
@@ -23,5 +23,3 @@ function AuthLayout({children, authentication=true}) {
     <div>{children}</div>
   )
 }
-
-export default AuthLayout
