3749a460d09b2fc580a8f0c6ed591990315eb129
nikhilkumar40 <108416519+nikhilkumar40@users.noreply.github.com>
2025-08-03 11:58:51 +0530

    added Editor and inegrated

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Input, Button, PostCard } from '../index'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
@@ -38,15 +38,79 @@ function PostForm(post) {
                 data.featuredImage = file.$id;
                 const dbPost = await dbConfigService.createPost({ ...data, userId: userData.$id })
 
-                if(dbPost){
+                if (dbPost) {
                     navigate(`post/${dbPost.$id}`)
                 }
             }
         }
     }
 
+    const slugTransform = useCallback((slug) => {
+        if (slug != '' && typeof (slug) == "string") {
+            return slug.trim().toLowerCase().replace(/[^a-zA-Z\d\s]+/g, "-")
+                .replace(/\s/g, "-");
+        }
+        return ''
+    }, [])
+
+    useEffect(() => {
+
+        const subscription = watch((value, { name }) => {
+            if (name == 'title') {
+                setValue('slug', slugTransform(value.title),
+                    { shouldValidate: true })
+            }
+        })
+
+    }, [slugTransform, setValue, watch])
     return (
-        <div>PostForm</div>
+           <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
+                />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+            </div>
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
     )
 }
 
